1e09d320b791a38f22ab0f2cdd315372da0fea24
Omo <okpebhoo@gmail.com>
2020-05-16 06:15:40 +0100

    penultimate front end clean-up and first push to master

---
+++ dashboard/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux';
+import store from './store';
+import {BrowserRouter} from 'react-router-dom'
+
+
+// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(()=>(store.getState()))
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store = {store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
